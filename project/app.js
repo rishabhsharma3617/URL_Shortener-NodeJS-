eb8e8ef601b6190ff9549ae91db83c2a6ac7a10e
Rishabh Sharma <rishabhsharma3617@gmail.com>
2020-02-17 15:09:26 +0530

    Added the dependencies

--- project/app.js
+++ project/app.js
@@ -1,4 +1,21 @@
-const app = require('app')
+const express = require('express')
+const connectDB = require('./config/db')
+const app = express()
+
+
+//Connect to database
+connectDB()
+
+
+app.use(express.json({extended : false}))
+
+//DEfine Routes
+
+app.use('/',require('./routes/index'))
+app.use('/api/url',require('./routes/url'))
+
+const PORT = 5000;
+app.listen(PORT , () => console.log(`Server is running on ${PORT}`))
 
 
 
